1645f9a6a5a0a634fa18f95ee4e96f5fe06221a8
oviola01 <o.viola01@gmail.com>
2023-06-01 19:40:43 +0200

    érdekes óra

---
+++ src/kozol/KonzolJop.java
@@ -0,0 +1,18 @@
+
+//OOP - öröklődés, polimorfizmus, egységbe zárás
+//polimorfizmus - többféle objektum ugyanannak tűnhet
+
+package kozol;
+
+import javax.swing.JOptionPane;
+
+public class KonzolJop {
+
+    public static void main(String[] args) {
+        //JOptionPane jop = new JOptionPane();
+        //jop.setVisible(true);
+        JOptionPane.showMessageDialog(null, "helló JOP!");
+
+    }
+    
+}
